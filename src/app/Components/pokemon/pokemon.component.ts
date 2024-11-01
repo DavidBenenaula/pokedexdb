5301e1d1e27df49b1aa2428ba9d5b9a045f62b4c
davidbenenaulaleon <davidbenenaulaleon@gmail.com>
2024-11-01 03:57:55 +0000

    first commit

---
+++ src/app/Components/pokemon/pokemon.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input,Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon.component.html',
+  styleUrl: './pokemon.component.css'
+})
+export class PokemonComponent {
+@Input({required:true}) solicitarImagen:string="";
+
+@Output() clickPokemon = new EventEmitter<number>();
+
+anteriorPokemon(){
+ this.clickPokemon.emit(-1);
+}
+
+siguientePokemon(){
+   this.clickPokemon.emit(1);
+
+  }
+
+
+
+}
+
